1647c77ea14668fc19c00a0ca539002aa4f27ae3
4723226 <4723226k@hgu.jp>
2026-01-19 05:21:07 +0000

    14

--- frontend/src/views/TodoListView.vue
+++ frontend/src/views/TodoListView.vue
@@ -1,24 +1,25 @@
 <script setup lang="ts">
     import { useTodosStore } from '../stores/todoStore';
     import AddTodo from '../components/AddTodo.vue';
+    import { RouterLink } from 'vue-router';
     const todoStore = useTodosStore();
 </script>
 
 <template>
 <section>
-    <h2>Todo List</h2>
+    <h2>Todos</h2>
     <ul>
-    <li
-        v-for="todo in todoStore.todos"
-        :key="todo.id"
-        style="display:flex; align-items:center; gap:0.5rem; margin:0.25rem 0;"
-    >
-        <input type="checkbox" v-model="todo.completed" />
-        <span :style="{ textDecoration: todo.completed ? 'line-through' : 'none' }">
-        {{ todo.title }}
-        </span>
-        <button v-on:click="todoStore.removeTodo(todo.id)">Delete</button>
-    </li>
+        <li
+            v-for="todo in todoStore.todos"
+            :key="todo.id"
+            style="display:flex; align-items:center; gap:0.5rem; margin:0.25rem 0;"
+        >
+            <input type="checkbox" v-model="todo.completed" />
+            <span :style="{ textDecoration: todo.completed ? 'line-through' : 'none' }">
+                <router-link :to="{name:'TodoDetail',params: { id: todo.id }}">{{ todo.title }}</router-link>
+            </span>
+            <button v-on:click="todoStore.removeTodo(todo.id)">Remove</button>
+        </li>
     </ul>
 </section>
 <section>
